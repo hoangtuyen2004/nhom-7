63bfa1b3f396363d7d1ff0fe1d24c2c040cdff6a
duongcvph36514 <caovanduong20@gmail.com>
2023-12-05 23:05:21 +0700

    register(đăng kí), comment client

--- routes.php
+++ routes.php
@@ -10,28 +10,24 @@ use Ductong\BaseMvc\Controllers\Admin\NoteController;
 use Ductong\BaseMvc\Controllers\Client\HomeController;
 use Ductong\BaseMvc\Controllers\Client\ListController;
 use Ductong\BaseMvc\Controllers\Client\LoginController;
-use Ductong\BaseMvc\Controllers\Client\WriterController;
-use Ductong\BaseMvc\Controllers\Client\WritingController;
 use Ductong\BaseMvc\Router;
 
 $router = new Router();
 
-// Người xem
 $router->addRoute('/', HomeController::class, 'index');
 $router->addRoute('/client/user', HomeController::class, 'user');
 
 $router->addRoute('/client/login', LoginController::class, 'login');
 $router->addRoute('/client/logout', LoginController::class, 'logout');
+$router->addRoute('/client/registers/index', LoginController::class, 'register');
+
 
 $router->addRoute('/client/list/news', ListController::class, 'listNews');
+$router->addRoute('/client/list/ccomment', ListController::class, 'ccomment');
 
-// Người viết
-$router->addRoute('/client/writer', WriterController::class, 'index');
-
-$router->addRoute('/client/writer/news', WritingController::class, 'index');
-
-// Quản trị viên
 $router->addRoute('/admin/dashboard', DashboardController::class, 'index');
+
+
 
 $router->addRoute('/admin/users', UserController::class, 'index');
 $router->addRoute('/admin/users/create', UserController::class, 'create');
--- src/Controllers/Client/LoginController.php
+++ src/Controllers/Client/LoginController.php
@@ -29,5 +29,41 @@ class LoginController extends Controller {
         session_destroy();
         header('location: /');
     }
+
+    public function register() {
+        $tb = "";
+        if (isset($_POST['btn_submit'])) {
+            $data = [
+                'name_account' => $_POST['name_account'],
+                'email' => $_POST['email'],
+                'password' => $_POST['password'],
+                'id_role' => $_POST['id_role']
+            ];
+
+            // Kiểm tra xem tên người dùng đã tồn tại hay chưa
+            $existingUser = $this->checkUser($data['name_account']);
+
+            if ($existingUser) {
+                // Nếu tên người dùng đã tồn tại, hiển thị thông báo và không thêm vào cơ sở dữ liệu
+                $tb = "Tên người dùng đã tồn tại. Vui lòng chọn một tên khác.";
+            } else {
+                // Nếu tên người dùng chưa tồn tại, thêm vào cơ sở dữ liệu
+                (new User())->insertTk($data);
+                header('location: /');
+            }
+        }
+
+        $this->render('client/registers/index', compact('tb'));
+    }
+
+    private function checkUser($nameAccount) {
+        $users = (new User())->all();
+        foreach ($users as $user) {
+            if ($user['name_account'] == $nameAccount) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 ?>
