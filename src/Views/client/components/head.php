eb4810110213995ff194098ed55efcd599adf885
dương trương <truongdz2903@gmail.com>
2023-12-09 16:49:26 +0700

    bổ sung

--- src/Views/client/components/head.php
+++ src/Views/client/components/head.php
@@ -1,3 +1,18 @@
+<link rel="stylesheet" type="text/css" href="/assets/files/bower_components/bootstrap/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="/assets/files/assets/pages/waves/css/waves.min.css" type="text/css" media="all">
+
+<link rel="stylesheet" type="text/css" href="/assets/files/assets/icon/feather/css/feather.css">
+
+<link rel="stylesheet" type="text/css" href="/assets/files/assets/css/font-awesome-n.min.css">
+
+<link rel="stylesheet" href="/assets/files/bower_components/chartist/css/chartist.css" type="text/css" media="all">
+
+<link rel="stylesheet" type="text/css" href="/assets/files/bower_components/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" type="text/css" href="/assets/files/bower_components/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css">
+
+<link rel="stylesheet" type="text/css" href="/assets/files/assets/css/style.css">
+<link rel="stylesheet" type="text/css" href="/assets/files/assets/css/widget.css">
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="icon" type="image/png" href="/asset-client/Image/N.png">
