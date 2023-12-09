4ee9ac412e1b7c1af715104ddf7c4eecc3f29ff1
hoangtuyen2004 <ductuyen772@gmail.com>
2023-12-09 21:55:42 +0700

    update

---
+++ src/Views/client/writer/comment/list.php
@@ -0,0 +1,81 @@
+<div class="pcoded-content">
+    <div class="page-header card">
+        <div class="row align-items-end">
+            <div class="col-lg-8">
+                <div class="page-header-title">
+                    <i class="feather icon-home bg-c-blue"></i>
+                    <div class="d-inline">
+                        <h5>Quản lý bình luận</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="page-header-breadcrumb">
+                    <ul class=" breadcrumb breadcrumb-title">
+                        <li class="breadcrumb-item">
+                            <a href="/client/writer/news"><i class="feather icon-home"></i></a>
+                        </li>
+                        <li class="breadcrumb-item"><a href=""></a>Bài viết</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="pcoded-inner-content">
+        <div class="main-body">
+            <div class="page-wrapper">
+                <div class="page-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card table-card">
+                                <div class="card-header">
+                                    <h5>Bài viết: <?=$news['id']?></h5>
+                                    <div class="card-header-right">
+                                        <a href="/client/writer/comment"
+                                            class="btn waves-effect waves-light btn-google-plus">
+                                            <i class="fa fa-times-circle fa-lg" style="color: #ffff;"></i> Quay lại
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class>
+                                    <div class="col-xl-12 col-md-12">
+                                        <div class="table-card">
+                                            <div class="card-block">
+                                                <div class="table-responsive">
+                                                    <table class="table table-hover m-b-0 without-header">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>
+                                                                    <div class="d-inline-block align-middle">
+                                                                        <img style="width: 50px;height: 50px;" src="/assets/files/assets/images/avatar-4.jpg"
+                                                                            alt="user image"
+                                                                            class="img-radius img-40 align-top m-r-15">
+                                                                        <div class="d-inline-block">
+                                                                            <h6>Shirley Hoe</h6>
+                                                                            <p class="text-muted m-b-0">Sales
+                                                                                executive , NY</p>
+                                                                        </div>
+                                                                    </div>
+                                                                </td>
+                                                                <td class="text-right">
+                                                                    <h6 class="f-w-700">$78.001<i
+                                                                            class="fas fa-level-down-alt text-c-red m-l-10"></i>
+                                                                    </h6>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
