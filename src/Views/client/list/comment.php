0d37b7a67dff6ab7cba24cca67315c02437a3b5b
duongcvph36514 <caovanduong20@gmail.com>
2023-12-11 17:56:50 +0700

    sửa lỗi hiển thị comment theo bài viết

--- src/Views/client/list/comment.php
+++ src/Views/client/list/comment.php
@@ -1,31 +1,27 @@
 <?php foreach ($comment as $c) : ?>
-    <div class="comment">
-        <?php
-        // Lấy thông tin về người viết
-        $writerName = '';  // Biến để lưu tên người viết
+    <?php
+    // Lấy thông tin về người viết
+    $writerName = '';  // Biến để lưu tên người viết
 
-        // Lặp qua danh sách người viết
-        foreach ($writing as $write) {
-            // Kiểm tra xem bình luận thuộc về người viết nào
-            if ($write['id_news'] == $news['id'] && $write['id_user'] == $c['id_user']) {
-                $writerName = '23';
-                foreach ($users as $user) {
-                        // Lấy tên người viết
-                        if ($write['id_user'] == $user['id']) {
-                                $writerName = $user['name_account'];
-                                break;
-                            }
-                        }
-                    }
+    // Lặp qua danh sách người viết
+    foreach ($writing as $write) {
+        // Kiểm tra xem bình luận thuộc về người viết nào và thuộc về bài viết hiện tại
+        if ($write['id_news'] == $news['id'] && $write['id_user'] == $c['id_user']) {
+            foreach ($users as $user) {
+                // Lấy tên người viết
+                if ($write['id_user'] == $user['id']) {
+                    $writerName = $user['name_account'];
+                    break;
                 }
-                    ?>
+            }
+        }
+    }
+    ?>
 
+    <div class="comment">
         <p><strong><?= $writerName ?></strong>: <?= $c['comment'] ?></p>
     </div>
 <?php endforeach ?>
-
-
-
 
 <?php
 // Kiểm tra xem người dùng đã đăng nhập hay chưa
@@ -33,7 +29,7 @@ if (!isset($_SESSION['id_user'])) {
     echo "<p style='color: red;'>Bạn cần đăng nhập để bình luận</p>";
 } else {
     // Nếu đã đăng nhập, hiển thị form bình luận
-?>
+    ?>
     <form action="/client/list/ccomment" method="post">
         <input type="hidden" name="id_news" value="<?= $news['id'] ?>">
         <!-- <input type="hidden" name="id_users" value="<?= $users['id'] ?>"> -->
